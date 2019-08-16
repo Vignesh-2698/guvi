985b8d5f69d4a07a5554f70dfcc283c460692872
Vignesh-2698 <53395992+Vignesh-2698@users.noreply.github.com>
2019-08-16 10:35:53 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,8 +1,7 @@
 number=int(input("enter the number"))
-while(number>0):
-    if(number%2==0):
-        print("Even")
-    elif(number%2!=0):
-        print("Odd")
-    else:
-        print("invalid")
+if(number%2==0):
+    print("Even")
+elif(number%2!=0):
+    print("Odd")
+else:
+    print("invalid")
